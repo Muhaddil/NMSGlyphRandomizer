d2b0e7b9bf418125756f6218b88f1842b020c557
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2024-12-05 01:27:13 +0100

    Update 1.2 - Bug fixes, new implementations and copy button!

--- JS/glyphgeneratorV3.js
+++ JS/glyphgeneratorV3.js
@@ -10,48 +10,122 @@ export const generateGlyphs = () => {
   return glyphs;
 };
 
-export const displayRandomGlyphs = () => {
-  // Get the button and output elements from the DOM
+document.addEventListener("DOMContentLoaded", () => {
   const generateButton = document.getElementById("generateButton");
+  const copyButton = document.getElementById("copyButton");
   const glyphOutput = document.getElementById("glyphOutput");
+  const glyphOutputHex = document.getElementById("glyphOutputHex");
 
-  // Disable the button
+  generateButton.disabled = false;
+  copyButton.disabled = true;
+  // copyButton.style.display = "none";
+
+  glyphOutput.innerHTML = "";
+  glyphOutputHex.innerHTML = "";
+});
+
+export const displayRandomGlyphs = () => {
+  const generateButton = document.getElementById("generateButton");
+  const copyButton = document.getElementById("copyButton");
+  const glyphOutput = document.getElementById("glyphOutput");
+  const glyphOutputHex = document.getElementById("glyphOutputHex");
+
   generateButton.disabled = true;
+  copyButton.disabled = true;
 
-  // Generate the glyphs
+  copyButton.classList.remove("visible");
+  // copyButton.classList.add("hidden");
+
   const glyphs = generateGlyphs();
+  const portalCodeText = i18next.t("portalCode");
 
-  // Display the glyphs in the output elements
-  // Each glyph is a span with the class 'glyph' and a unique id
   glyphOutput.innerHTML = Array.from(
     { length: 12 },
     (_, i) => `<span id="glyph${i}" class="glyph"></span>`
   ).join("");
 
-  // Add the animation to each glyph
+  glyphOutputHex.innerHTML = `
+    <span id="portalText" class="glyphHex">${portalCodeText}</span>
+    ${Array.from(
+      glyphs,
+      (_, i) => `<span id="glyphHex${i}" class="glyphHex"></span>`
+    ).join("")}
+  `;
+
   glyphs.split("").forEach((glyph, i) => {
     setTimeout(() => {
       const glyphElement = document.getElementById(`glyph${i}`);
       glyphElement.classList.add("spin");
-      // Change the glyph every 100ms while it's spinning
+
       const intervalId = setInterval(() => {
         glyphElement.textContent = randomGlyph();
       }, 100);
-      // Stop changing the glyph after 1s
+
       setTimeout(() => {
         clearInterval(intervalId);
-        // Set the final glyph
         glyphElement.textContent = glyph;
-        // Enable the button after the last glyph has finished spinning
+
         if (i === glyphs.length - 1) {
           generateButton.disabled = false;
+          copyButton.disabled = false;
+
+          copyButton.classList.remove("hidden");
+          copyButton.classList.add("visible");
         }
       }, 1000);
-    }, i * 200); // Delay the start of each animation
+    }, i * 200);
+
+    setTimeout(() => {
+      const glyphHexElement = document.getElementById(`glyphHex${i}`);
+      glyphHexElement.classList.add("spin");
+
+      const intervalIdHex = setInterval(() => {
+        glyphHexElement.textContent = randomGlyph();
+      }, 100);
+
+      setTimeout(() => {
+        clearInterval(intervalIdHex);
+        glyphHexElement.textContent = glyph;
+      }, 1000);
+    }, i * 200);
   });
+
+  copyButton.dataset.clipboard = glyphs;
 };
 
-// Make the displayRandomGlyphs function globally accessible
+export const copyToClipboard = () => {
+  const copyButton = document.getElementById("copyButton");
+  const textToCopy = copyButton.dataset.clipboard;
+
+  navigator.clipboard.writeText(textToCopy).then(
+    () => {
+      showNotification(i18next.t("copiedSuccess"), "success");
+    },
+    () => {
+      showNotification(i18next.t("copiedFail"), "error");
+    }
+  );
+};
+
+const showNotification = (message, type) => {
+  let notification = document.createElement("div");
+  notification.className = `notification ${type === "error" ? "error" : ""}`;
+  notification.textContent = message;
+
+  document.body.appendChild(notification);
+
+  setTimeout(() => {
+    notification.classList.add("visible");
+  }, 10);
+
+  setTimeout(() => {
+    notification.classList.remove("visible");
+    setTimeout(() => notification.remove(), 500);
+  }, 3000);
+};
+
+window.displayRandomGlyphs = displayRandomGlyphs;
+window.copyToClipboard = copyToClipboard;
 window.displayRandomGlyphs = displayRandomGlyphs;
 
 // Function to populate the language options
@@ -76,20 +150,27 @@ const populateLanguageOptions = () => {
 window.changeLanguage = () => {
   const lang = document.getElementById("lang").value;
   i18next.changeLanguage(lang);
-  localStorage.setItem("i18nextLng", lang); 
+  localStorage.setItem("i18nextLng", lang);
   $("body").localize();
 };
 
 // Initialize i18next
 i18next.init(
   {
-    lng: localStorage.getItem("i18nextLng") || navigator.language || "en",
+    lng:
+      localStorage.getItem("i18nextLng") ||
+      navigator.language.split("-")[0] ||
+      "en", // Language fallback to browser language
     resources: {
       en: {
         translation: {
           title: "NMS Glyph Generator",
           header: "NMS Glyph Generator",
           button: "Generate Glyphs",
+          portalCode: "Portal Code:",
+          copyButton: "Copy",
+          copiedSuccess: "Code copied to clipboard!",
+          copiedFail: "Error copying the code.",
         },
       },
       es: {
@@ -97,6 +178,10 @@ i18next.init(
           title: "Generador de Glifos para NMS",
           header: "Generador de Glifos para NMS",
           button: "Generar Glifos",
+          portalCode: "Código del Portal:",
+          copyButton: "Copiar",
+          copiedSuccess: "¡Código copiado al portapapeles!",
+          copiedFail: "Error al copiar el código.",
         },
       },
       fr: {
@@ -104,6 +189,10 @@ i18next.init(
           title: "Générateur de glyphes NMS",
           header: "Générateur de glyphes NMS",
           button: "Générer des glyphes",
+          portalCode: "Code du portail :",
+          copyButton: "Copier",
+          copiedSuccess: "Code copié dans le presse-papiers !",
+          copiedFail: "Erreur lors de la copie du code.",
         },
       },
       de: {
@@ -111,6 +200,10 @@ i18next.init(
           title: "NMS Glyph Generator",
           header: "NMS Glyph Generator",
           button: "Glyphs generieren",
+          portalCode: "Portal-Code:",
+          copyButton: "Kopieren",
+          copiedSuccess: "Code in die Zwischenablage kopiert!",
+          copiedFail: "Fehler beim Kopieren des Codes.",
         },
       },
       eu: {
@@ -118,6 +211,10 @@ i18next.init(
           title: "NMS Glifo Sortzailea",
           header: "NMS Glifo Sortzailea",
           button: "Sortu Glifoak",
+          portalCode: "Atariaren Kodea:",
+          copyButton: "Kopiatu",
+          copiedSuccess: "Kodea ondo kopiatu da!",
+          copiedFail: "Errorea kodea kopiatzerakoan.",
         },
       },
     },
@@ -132,7 +229,8 @@ i18next.init(
     populateLanguageOptions();
 
     // This selects the correct language option in the select
-    const lang = localStorage.getItem("i18nextLng") || i18next.language; // Get the selected language from local storage
+    const lang =
+      localStorage.getItem("i18nextLng") || i18next.language.split("-")[0]; // Get the selected language from localStorage or navigator.language
     document.getElementById("lang").value = lang;
   }
 );
